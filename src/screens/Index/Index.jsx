53b5286f4fb7246143ea6d14393b2a2defbf1c62
Rutuja Deshmukh <66289022+develop-er11@users.noreply.github.com>
2023-09-02 21:54:52 +0530

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="home-page">
+            <div className="overlap-group">
+              <div className="div">
+                <div className="ellipse" />
+                <div className="doodles-mixed-round">
+                  <div className="overlap-group-2">
+                    <img
+                      className="glasses"
+                      alt="Glasses"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/glasses-1@2x.png"
+                    />
+                    <img
+                      className="img"
+                      alt="Glasses"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/glasses-2@2x.png"
+                    />
+                    <img
+                      className="keyboard"
+                      alt="Keyboard"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/keyboard-1@2x.png"
+                    />
+                    <img
+                      className="food"
+                      alt="Food"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/food-1@2x.png"
+                    />
+                    <img
+                      className="clock"
+                      alt="Clock"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/clock-1@2x.png"
+                    />
+                    <img
+                      className="python"
+                      alt="Python"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/python-1@2x.png"
+                    />
+                    <img
+                      className="javascript-init"
+                      alt="Javascript init"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/javascript-init-1@2x.png"
+                    />
+                    <img
+                      className="html"
+                      alt="Html"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/html-1@2x.png"
+                    />
+                    <img
+                      className="react"
+                      alt="React"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/react-1@2x.png"
+                    />
+                    <img
+                      className="node"
+                      alt="Node"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/node-1@2x.png"
+                    />
+                    <img
+                      className="css"
+                      alt="Css"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/css-1@2x.png"
+                    />
+                    <img
+                      className="expres"
+                      alt="Expres"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/expres-1@2x.png"
+                    />
+                    <img
+                      className="macbook"
+                      alt="Macbook"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/macbook-1@2x.png"
+                    />
+                    <img
+                      className="vector"
+                      alt="Vector"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/vector-67-1@2x.png"
+                    />
+                    <img
+                      className="vector-2"
+                      alt="Vector"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/vector-67-1@2x.png"
+                    />
+                    <img
+                      className="lightbulb"
+                      alt="Lightbulb"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/lightbulb-1@2x.png"
+                    />
+                    <img
+                      className="apis"
+                      alt="Apis"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/apis-1@2x.png"
+                    />
+                    <img
+                      className="aws"
+                      alt="Aws"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/aws-1@2x.png"
+                    />
+                    <img
+                      className="goland"
+                      alt="Goland"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/goland-1@2x.png"
+                    />
+                    <img
+                      className="gutiar"
+                      alt="Gutiar"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/gutiar-1@2x.png"
+                    />
+                    <img
+                      className="SPRINKLES"
+                      alt="Sprinkles"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/sprinkles-1@2x.png"
+                    />
+                  </div>
+                </div>
+                <img
+                  className="line"
+                  alt="Line"
+                  src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/line-2-1@2x.png"
+                />
+                <img
+                  className="computer-login-pana"
+                  alt="Computer login pana"
+                  src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/computer-login-pana-1-1@2x.png"
+                />
+              </div>
+              <img
+                className="vector-3"
+                alt="Vector"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/vector-187-1@2x.png"
+              />
+            </div>
+          </div>
+          <div className="about-me-page">
+            <div className="overlap-2">
+              <div className="ellipse-2" />
+              <img
+                className="doodle-items"
+                alt="Doodle items"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/doodle-items-1@2x.png"
+              />
+              <img
+                className="group"
+                alt="Group"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/group-2350-1@2x.png"
+              />
+              <img
+                className="attached-files-pana"
+                alt="Attached files pana"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/attached-files-pana-1-1@2x.png"
+              />
+            </div>
+            <img
+              className="lightbulb-2"
+              alt="Lightbulb"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/lightbulb-2@2x.png"
+            />
+            <img
+              className="vector-4"
+              alt="Vector"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/vector-186-1@2x.png"
+            />
+          </div>
+          <img
+            className="projects-page"
+            alt="Projects page"
+            src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/projects-page-1@2x.png"
+          />
+          <div className="contact-page">
+            <div className="overlap-3">
+              <div className="ellipse-3" />
+              <img
+                className="vector-5"
+                alt="Vector"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/vector-186-2@2x.png"
+              />
+              <img
+                className="angle-brace"
+                alt="Angle brace"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/angle-brace-1@2x.png"
+              />
+              <img
+                className="working-pana"
+                alt="Working pana"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/working-pana-1-1@2x.png"
+              />
+            </div>
+            <img
+              className="mail"
+              alt="Mail"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/mail-1@2x.png"
+            />
+            <img
+              className="keyboard-2"
+              alt="Keyboard"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64ee2ef466b2aab0be2dda7d/releases/64ee2fc2f4d2dc5a9f80f1b0/img/keyboard-2@2x.png"
+            />
+          </div>
+          <div className="footer-page" />
+        </div>
+      </div>
+    </div>
+  );
+};
